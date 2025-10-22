9ba7eda9ec1c08876bbac409ff52748c71d12e00
r03iuL <robiulislam1806@gmail.com>
2025-10-22 06:13:35 +0600

    function -2

--- functions.js
+++ functions.js
@@ -11,44 +11,101 @@
 // 7.10 Function Composition
 
 function add(a, b) {
-  return a + b;
+    return a + b;
 }
 
 let res = add(10, 20);
 console.log(res); //30
 
 function calculate(fn, ...args) {
-  return fn(...args);
+    return fn(...args);
 }
 
 console.log(calculate(add, 15, 20)); //35
 
-
 //Return Values (Basic & Objects)-----------------------------
 //functions can return number , string ,boolean , null ,undefined etc
 
-//fuction can return object
-function claculator() {
-  return {
-    addition: function (a, b) {
-      return a + b;
-    },
+//function can return object
+function calculator() {
+    return {
+        addition: function (a, b) {
+            return a + b;
+        },
 
-    substraction: function (a, b) {
-      return a - b;
-    },
-  };
+        subtraction: function (a, b) {
+            return a - b;
+        },
+    };
 }
 
-console.log(claculator().addition(25, 20)); //45
+console.log(calculator().addition(25, 20)); //45
 
-//fuction can return function
+//function can return function
 function multiplier() {
-  return function (a, b) {
-    return a * b;
-  };
+    return function (a, b) {
+        return a * b;
+    };
 }
 
 let result = multiplier();
 console.log("7 Times 5 is  ", result(7, 5));
 
+//callback function----------------------------
+
+function greet(name) {
+    console.log("Hello " + name + " ,welcome!");
+}
+
+function inputProcessor(input, fn) {
+    fn(input);
+}
+inputProcessor("Rakib", greet);
+
+function showMessage() {
+    console.log(" DownLoad Successful!");
+}
+
+function download(fn) {
+    console.log("Downloading .......");
+
+    setTimeout(() => {
+        fn();
+    }, 2000);
+}
+
+download(showMessage);
+
+//function expression
+
+let multiply = function (a, b) {
+    return a * b;
+};
+
+console.log(multiply(10, 345));
+
+//arrow function
+
+let arrowAdd = (a, b) => a + b;
+
+console.log(arrowAdd(334, 443));
+
+let isEven = (num) => num % 2 == 0;
+
+console.log(isEven(23947));
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9.1];
+
+function evnChecker(numArray, fn) {
+    console.log("Checkcing .....");
+
+    numArray.map((num) => {
+        if (fn(num)) {
+            console.log(num, " is Even");
+        } else {
+            console.log(num, " is Odd.");
+        }
+    });
+}
+
+evnChecker(numbers, isEven);
